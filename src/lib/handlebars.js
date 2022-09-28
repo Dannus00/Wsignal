703190e7d1a765a299c5cdad398ac35e0910f2d0
dannus00 <danusdiaz0@gmail.comgit config --global user.email danusdiaz0@gmail.com>
2022-09-28 00:24:57 -0500

    Version 1.4

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,5 +1,6 @@
 const {format} = require('timeago.js');
-const Handlebars = require('handlebars')
+const Handlebars = require('handlebars');
+const moment = require('moment');
 
 
 const helpers = {};
@@ -11,6 +12,11 @@ helpers.timeago = (timestamp)=>{
 
 Handlebars.registerHelper('encodeMyString',function(inputData){
     return new Handlebars.SafeString(inputData);
+});
+
+Handlebars.registerHelper('formatTime', function (date, format) {
+    var mmnt = moment(date);
+    return mmnt.format(format);
 });
 
 module.exports = helpers;
